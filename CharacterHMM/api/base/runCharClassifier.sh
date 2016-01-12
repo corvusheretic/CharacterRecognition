aafdcda56d149c9a79c0ba6e7682cae80c2629fd
corvusheretic <kalyanram.a@gmail.com>
2016-01-12 20:11:41 +0100

    Run script modified

--- CharacterHMM/api/base/runCharClassifier.sh
+++ CharacterHMM/api/base/runCharClassifier.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-python charClassifier.py ns 14 4.2 bw aup
-python charClassifier.py mb 50 10 bw aup
-python charClassifier.py ns 14 4.2 bk aup
-python charClassifier.py mb 50 10 bk aup
+python charClassifier.py ns 14 4.2 bw ucc
+python charClassifier.py mb 50 10 bw ucc
+python charClassifier.py ns 14 4.2 bk ucc
+python charClassifier.py mb 50 10 bk ucc
+# These are longer tests
+#python charClassifier.py ns 14 4.2 bw aup
+#python charClassifier.py mb 50 10 bw aup
+#python charClassifier.py ns 14 4.2 bk aup
+#python charClassifier.py mb 50 10 bk aup
